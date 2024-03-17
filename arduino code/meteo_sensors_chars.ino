a596ae21eac7111c0e7fd43310913aa8a0ee8c97
Arduinum <message.chaos628@gmail.com>
2024-03-17 18:45:46 +0300

    added: arduino code for hex and chars input

---
+++ arduino code/meteo_sensors_chars.ino
@@ -0,0 +1,22 @@
+// библиотека для работы с аналоговым термометром (Troyka-модуль)
+#include <TroykaThermometer.h>
+
+// создаём объект для работы с аналоговым термометром
+// и передаём ему номер пина выходного сигнала
+TroykaThermometer thermometer(A0);
+
+void setup()
+{
+    // открываем последовательный порт
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    // считываем данные с аналогового термометра
+    thermometer.read();
+    // вывод показателей аналогового термометра в градусах Цельсия
+    int data = thermometer.getTemperatureC();
+    Serial.println(char(data) + " C");
+    delay(1000);
+}
